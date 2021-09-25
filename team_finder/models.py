fccee88068147ac25635b7308eaa9312d58923ea
abcoste2 <abcoste2@ncsu.edu>
2021-09-25 14:01:34 -0400

    add models fix

--- team_finder/models.py
+++ team_finder/models.py
@@ -1,6 +1,24 @@
 from django.db import models
+from django.db.models.fields import CharField, TextField
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
-class candidate(models.Model) {
-    
-}
+class candidate(models.Model):
+    full_name =  models.CharField()
+    title = models.CharField()
+    email = models.EmailField()
+    rate = models.FloatField()
+    years_exp = models.IntegerField()
+    skills = models.ForeignKey(Skill, on_delete=models.CASCADE)
+    current_proj = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+class Skill(models.Model):
+    # TODO
+    dummyvalue = models.NullBooleanField()
+
+class Project(models.Model):
+    name = models.CharField()
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    skills_req = models.ForeignKey(Skill, on_delete=models.CASCADE)
