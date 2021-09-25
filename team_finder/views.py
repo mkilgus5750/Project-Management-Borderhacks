83d76b5836d32ffcdd537b7454823e508dcd5655
Mari Kilgus <mkilgus@ncsu.edu>
2021-09-25 16:48:42 -0400

    planning: match_team

--- team_finder/views.py
+++ team_finder/views.py
@@ -5,6 +5,20 @@ from .models import Project, Candidate
 
 # Create your views here.
 
+all_projects = Project.objects.all()
+
+def match_team(slug):
+#   1) Find available candidates
+#      - If a candidate is assigned to a project that conflicts with the one we are concerned with, they not considered
+#   2) for skill in candidate.skills if candidates_skills in required_skills then add to consideration
+#   3) Find all the combinations of size n that meet the skill requirements starting from n=1 and going up until you have several team options
+#   4) Find the min/max of target attributes (price, experience, etc)
+    Project.objects.get(slug=slug)
+    all_candidates = Candidate.objects.all()
+    for candidate in all_candidates:
+        pass
+
+
 def index(request):
     return render(request, 'team_finder/index.html')
 
