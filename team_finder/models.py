ec48bcb6062d5b16cd036b8e62cd246d9d5ca767
Mari Kilgus <mkilgus@ncsu.edu>
2021-09-26 02:17:26 -0400

    feat: styling

--- team_finder/models.py
+++ team_finder/models.py
@@ -4,9 +4,14 @@ from django.db.models.fields import CharField, TextField
 # Create your models here.
 class Skill(models.Model):
     name = models.CharField(null=True, max_length=100)
-
     def __str__(self):
         return self.name
+
+class Team(models.Model):
+    members = models.ManyToManyField(Candidate)
+    avg_experience = models.FloatField()
+    total_rate = models.FloatField()
+    total_skills = models.ManyToManyField(Skill)
 
 class Project(models.Model):
     name = models.CharField(max_length=100)
@@ -14,6 +19,7 @@ class Project(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
     skills_req = models.ManyToManyField(Skill)
+    # team = models.ForeignKey(Team, on_delete=models.CASCADE, null=True)
     slug = models.SlugField(null=True, db_index=True)
     def __str__(self):
         return self.name
@@ -31,9 +37,7 @@ class Candidate(models.Model):
     def __str__(self):
         return self.first_name + ' ' + self.last_name
 
-class Team(models.Model):
-    members = models.ManyToManyField(Candidate)
-    avg_experience = models.FloatField()
-    total_rate = models.FloatField()
-    total_skills = models.ManyToManyField(Skill)
+
+
+
 
